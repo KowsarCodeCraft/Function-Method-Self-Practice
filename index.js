aae23b19929f56deadea12eb908b5fb0bda8127d
Kowsar Islam <kowsarulislam@gmail.com>
2024-03-01 23:36:11 +0200

    few code added

---
+++ index.js
@@ -0,0 +1,18 @@
+//function myFunction(){
+    //console.log("Learning Function in JS");
+//} 
+//myFunction();// here this is function call system to see the outcome.
+
+//function sum(x, y) {
+    //console.log(x+y);
+//}
+//sum(5,15);
+
+//If we use return key word in the function then it will return the required value. 
+
+function sum(x, y) {
+    s = x + y
+    return s;
+}
+let val = sum(5,15);
+console.log(val);
